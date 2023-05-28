27ee6871bd26f3680f6d1d3be5b413628365b1a4
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-05-28 18:07:43 +0300

    Alex

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import { Section } from './components/Section/Section';
 import { FeedbackOptions } from './components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from './components/Statistics/Statistics';
-// import user from './components/Data/user.json';
-
 export default class App extends Component {
   state = {
     good: 0,
